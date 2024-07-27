d61574bc898c9dc3129ac101fc2ef9e50079513d
Margaret Hansen <margareh@andrew.cmu.edu>
2024-07-27 12:44:51 -0700

    add explicit dynamics model to example

--- example.py
+++ example.py
@@ -6,6 +6,7 @@ from scipy.stats import multivariate_normal as norm
 import matplotlib.pyplot as plt
 
 from ergodic_search import erg_planner
+from ergodic_search.dynamics import DiffDrive
 
 # map settings
 LOCS = [[0.25, 0.25], [0.85, 0.5], [0.6, 0.85]]
@@ -38,18 +39,31 @@ if __name__ == "__main__":
     # parse arguments
     args = erg_planner.ErgArgs()
 
-    # set a more interesting starting position
+    # set a more interesting starting position and initial controls
     args.start_pose = [0.2, 0.4, 0]
     args.num_freqs = 10
+
+    init_controls = np.zeros((args.traj_steps,2))
+    dx = (0.8 - 0.2) / args.traj_steps
+    init_controls[:,0] = dx
+    print(init_controls)
+
+    # create dynamics module so we can test it
+    diff_drive = DiffDrive(args.start_pose, args.traj_steps)
+    traj = diff_drive.forward(init_controls)
+    print(traj)
+
+    controls = diff_drive.inverse(traj)
+    print(controls)
 
     # create example map
     map = create_map(args.num_pixels)
 
     # initialize planner
-    planner = erg_planner.ErgPlanner(args, map)
+    planner = erg_planner.ErgPlanner(args, map, init_controls=init_controls, dyn_model=diff_drive)
 
     # generate a trajectory
-    traj = planner.compute_traj(debug=True)
+    traj = planner.compute_traj(debug=args.debug)
 
     # visualize map and trajectory
     planner.visualize()
